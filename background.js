df2b4ce940717d3819ec6f89407dc82901c48e75
Matt Krins <mattkrins@gmail.com>
2018-03-13 12:18:49 +1100

    Version 1.2
    Added date extraction.

--- background.js
+++ background.js
@@ -2,12 +2,27 @@ var context = "selection";
 var title = "Add to Calendar";
 var id = chrome.contextMenus.create({"title": title, "contexts":[context],"id": "context" + context});  
 
+function findDates(str) {
+	var res = str.match(/\d{2}([\/.-])\d{2}\1\d{4}/g);
+	if(!str || !res || !res[0]){return false;}
+	var dts = [];
+	for (i = 0; i < res.length; i++) { 
+		var d = new Date(res[i]);
+		if (isNaN(Date.parse(d))){continue;}
+		var ds = d.getFullYear().toString() + 
+		('0'+(d.getMonth()+1)).slice(-2) + 
+		('0'+d.getDate()).slice(-2);
+		dts.push(ds);
+	} if(dts.length <= 0){return false;} return dts;
+}
 
 function onClickHandler(info, tab) {
   var sText = info.selectionText;
-  var url = "https://www.google.com/calendar/event?action=TEMPLATE&details="+"Added From: \n"+tab.url+"&text=" + encodeURIComponent(sText);  
+  var url = "https://www.google.com/calendar/event?action=TEMPLATE&details="+"Added From: \n"+tab.url+"&text=" + encodeURIComponent(sText);
+  var date = findDates(sText);
+  if (date) { url = url+"&dates="+date[0]+"/"; }
+  if (date[1]) { url = url+date[1]; } else { url = url+date[0]; }
   window.open(url, '_blank');
-  console.log('Opening Google Calendar with: "' + sText+'"');
 };
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
